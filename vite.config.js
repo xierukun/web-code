7b6c1c877c71f6652adae6275077bf26f78bef1e
xierukun <xierukun@gmail.com>
2023-08-01 01:18:54 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+export default defineConfig({
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  server: {
+    // 配置反向代理
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
